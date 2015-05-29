8d76e76bfa73a838e673482fafd8cb7286420848
matt venn <matt@mattvenn.net>
2015-05-29 15:12:46 +0100

    ui & lcd seem to be working

---
+++ software/rfid_power_controller/tools.ino
@@ -0,0 +1,41 @@
+
+
+// placeholder, will be fetched from google spreadsheet!
+void setup_users()
+{
+    //0
+    users[0].user_name = "matt";
+    users[0].rfid = "04184A4FC6";
+
+    //1
+    users[1].user_name = "nic";
+    users[1].rfid = "04184A4147";
+
+}
+
+
+    
+// placeholder, will be fetched from google spreadsheet!
+void setup_tools()
+{
+    tools[0].tool_name = "lathe";
+    int t0[] = { 0, 1 };
+    memcpy(tools[0].users, t0, sizeof t0);
+
+    tools[1].tool_name = "mill";
+    tools[1].operational = false;
+    int t1[] = { 0, 1 };
+    memcpy(tools[1].users, t1, sizeof t1);
+
+    tools[2].tool_name = "laser";
+    tools[2].running = true;
+    tools[2].current_user = 0;
+    tools[2].operational = true;
+    tools[2].time = 1000;
+    int t2[] = { 0, 1 };
+    memcpy(tools[2].users, t2, sizeof t2);
+
+    tools[3].tool_name = "saw";
+    int t3[] = { 0 };
+    memcpy(tools[3].users, t3, sizeof t3);
+}
