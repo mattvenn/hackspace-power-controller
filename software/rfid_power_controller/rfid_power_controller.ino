b983969f97e5f2874bc0e6fc88ef7b7bf1134110
matt venn <matt@mattvenn.net>
2015-05-29 17:02:34 +0100

    logic and bugfixes

--- software/rfid_power_controller/rfid_power_controller.ino
+++ software/rfid_power_controller/rfid_power_controller.ino
@@ -3,65 +3,142 @@
 #define ENCA_b 7
 #define RFID_RX 4
 #define RFID_TX RFID_RX //same as each other as we never send
-
-#define USE_ENCODER
+#define BUT 2
+#define BUT_LED 5
+#define RADIO_TX 3
 #include <LiquidCrystal.h>
+
+//UI defs
+#define LCD_TIMEOUT 500 //time we spend before changing screen
 
 // initialize the library with the numbers of the interface pins
 //rs enable d4,5,6,7
 LiquidCrystal lcd(8,9,10,11,12,13);
 
+#define num_tools 4
+#define num_users 2
+
+struct tool
+{
+    String tool_name;
+    bool running = false;
+    int current_user = 0;
+    bool operational = true;
+    int time = 0;
+    int users[num_users];
+};
+
+struct user
+{
+    String user_name;
+    String rfid;
+};
+
+struct tool tools[num_tools];
+struct user users[num_users];
+
+int enc_clicks = 0; //encoder enc_clicks
+int tool_id = 0; //what page is showing on lcd
+int user_id = -1;
 
 void setup()
 {
-  Serial.begin(9600); 
-  Serial.println("started");
-  setup_lcd();
-  setup_radio();
-  setup_rfid();
+    Serial.begin(9600); 
+    Serial.println("started");
+    setup_lcd();
+    setup_radio();
+    setup_rfid();
+
+    //button setup
+    pinMode(BUT, INPUT);
+    digitalWrite(BUT, HIGH);
+    pinMode(BUT_LED, OUTPUT);
+    digitalWrite(BUT_LED, LOW);
+
+    //fetch users & tools
+    setup_tools();
+    setup_users();
+
+    lcd_start();
 }
-
-int count = 0;
-
-String valid = "04184A4FC6";
-//04184A4147
-
-typedef struct
-{
-    unsigned int fuelcellStatus;
-    float fuelcellAmbientT;
-    float fuelcellStackV;
-} Message;
-Message message;
 
 void loop() 
 {
-  #ifdef USE_ENCODER
-  check_encoder();
-  #endif
+    //check for rfid
+    String rfid = check_rfid();
+    if(rfid != "")
+    {
+        user_id = get_user_id(rfid);
+        //valid user id?
+        if(user_id >=0)
+        {
+            Serial.println("valid id");
+            //say hi
+            lcd_valid_user(user_id);
+            delay(LCD_TIMEOUT);
+            tool_id = 0;
+            lcd_show_tool(tool_id, user_id);
+            enc_clicks = 0;
+        }
+        else
+        {
+            lcd_invalid_user();
+            delay(LCD_TIMEOUT);
+            lcd_start();
+        }
+    }
 
-  String id = check_rfid();
-  if(id == valid)
-  {
-    Serial.println("valid id");
-  }
-    
+    //we have a valid user, so let them use the encoder to select tools
+    if(user_id >= 0)
+    {
+        check_encoder();
+        //limit to num of tools available
+        if(enc_clicks < 0)
+            enc_clicks = 0;
+        if(enc_clicks > num_tools - 1)
+            enc_clicks = num_tools - 1;
   
-    // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis()/1000);
-  lcd.setCursor(10,2);
-  lcd.print(count);
-  if( count == 10 )
-  {
-    turn_on();
-  }
-  if(count == 11 )
-  {
-      turn_off();
-  }
+        //different to last time? update display
+        if(tool_id != enc_clicks)
+        {
+            tool_id = enc_clicks;
+            Serial.print("showing tool id: ");
+            Serial.println(enc_clicks);
+            lcd_show_tool(tool_id, user_id);
+        }
 
+        //we only care about button presses if a valid user & inducted
+        if(is_inducted(user_id, tool_id) && tools[tool_id].operational)
+        {
+            if(digitalRead(BUT) == LOW)
+            {
+                Serial.println("button pressed");
+                //running
+                if(tools[tool_id].running)
+                {
+                    //only turn off if we're the current user
+                    if(tools[tool_id].current_user == user_id)
+                    {
+                        tools[tool_id].running = false;
+                        //log time & turn off
+                        radio_turn_off(tool_id);
+                        Serial.println("logging tool off time");
+                    }
+                }
+                else
+                {
+                    //log time & turn on
+                    tools[tool_id].running = true;
+                    tools[tool_id].time = millis()/1000;
+                    tools[tool_id].current_user = user_id;
+                    Serial.println("logging tool on time");
+                    radio_turn_on(tool_id);
+                }
+
+                //update lcd
+                lcd_show_tool(tool_id, user_id);
+            }
+        }
+    }
 }
 
