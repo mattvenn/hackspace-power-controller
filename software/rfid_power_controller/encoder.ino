c5ec03d33471cd548d8b5d45ba1f02346c42b579
matt venn <matt@mattvenn.net>
2015-05-29 17:01:21 +0100

    varname

--- software/rfid_power_controller/encoder.ino
+++ software/rfid_power_controller/encoder.ino
@@ -1,13 +1,14 @@
-#ifdef USE_ENCODER
 #include <Rotary.h>
+
 Rotary r = Rotary(ENCA_a, ENCA_b);
 
-int clicks = 0;
 void check_encoder()
 {
   unsigned char result = r.process();
   if (result) {
-    Serial.println(result == DIR_CW ? "Right" : "Left");
+    if(result == DIR_CW)
+        enc_clicks ++;
+    else
+        enc_clicks --;
   }
 }
-#endif
