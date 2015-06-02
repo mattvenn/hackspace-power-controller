dd1c12853d2b7199b3f8552c00bbbe523b5b5694
matt venn <matt@mattvenn.net>
2015-06-02 08:45:00 +0100

    fixed rfid serial that broke with string tidy

--- software/power_controller/rfid.ino
+++ software/power_controller/rfid.ino
@@ -1,6 +1,7 @@
 #include <SoftwareSerial.h>
 #define RFID_ID_LENGTH 10 + 2
 SoftwareSerial rfid_serial(RFID_RX, RFID_TX); // RX, TX
+String query_cmd = "/root/python_bridge/fetch.py";
 
 void setup_rfid()
 {
@@ -14,11 +15,15 @@ void setup_rfid()
 String read_rfid()
 {
     String rfid = "";
+    Serial.println(F("checking rfid"));
     if(rfid_serial.available() == RFID_ID_LENGTH)
     {
-        //get rid of first header char
-        rfid_serial.read();
-        rfid = rfid_serial.readString();
+        while(rfid_serial.available())
+        {
+            char c = rfid_serial.read();
+            rfid.concat(c);
+        }
+        //trim newline
         rfid.trim();
         Serial.print("got RFID: "); Serial.println(rfid);
     }
@@ -37,9 +42,11 @@ void timeout_user()
 bool auth_user(String rfid)
 {
     Process p;
-    String command = "/root/spreadsheet/fetch.py --check-user --rfid " + rfid;
-    Serial.print(command);
-    p.runShellCommand(command);
+    p.begin(query_cmd);
+    p.addParameter("--check-user");
+    p.addParameter("--rfid");
+    p.addParameter(rfid);
+    p.run();
 
     if(p.exitValue() != 0)
         return false;
@@ -49,7 +56,7 @@ bool auth_user(String rfid)
     if(p.available())
     {
         user.name = p.readStringUntil(',');
-        Serial.print("got name: "); Serial.println(user.name);
+        Serial.print("name:"); Serial.println(user.name);
         int i = 0;
         while(p.available() > 1) //more than one means we skip the \n
         {
@@ -63,14 +70,14 @@ bool auth_user(String rfid)
 
 int get_tools()
 {
-    Serial.println("fetching list of tools");
+    Serial.println("fetch tools");
     Process p;
-    String command = "/root/spreadsheet/fetch.py --list-tools";
-    Serial.println(command);
-    p.runShellCommand(command);
+    p.begin(query_cmd);
+    p.addParameter("--list-tools");
+    p.run();
 
     if(p.exitValue() != 0)
-        Serial.print("error fetching tools");
+        Serial.print("error");
 
     int num_tools = 0;
     if(p.available())
@@ -85,7 +92,7 @@ int get_tools()
             //chomp the newline
             p.read();
 
-            Serial.print("tool id: "); Serial.print(tools[num_tools].id);
+            Serial.print("tool id:"); Serial.print(tools[num_tools].id);
             Serial.print(" name:"); Serial.println(tools[num_tools].name);
             num_tools++;
         }
@@ -105,9 +112,9 @@ bool is_inducted(int tool_id)
 
 void log_tool_time()
 {
-    Serial.println("logging tool off time");
+    Serial.println(F("logging time"));
     Process p;
-    p.begin("/root/spreadsheet/fetch.py");
+    p.begin(query_cmd);
     p.addParameter("--log-tool");
     p.addParameter(tools[page_num].name);
     p.addParameter("--rfid");
@@ -115,6 +122,25 @@ void log_tool_time()
     p.addParameter("--time");
     p.addParameter(lcd_format_time(tools[page_num].time));
     p.run();
+    Serial.println("run");
+    Serial.println(free_memory());
     //async so we don't have to wait - didn't work because p goes out of scope
 //    p.runAsynchronously();
 }
+
+extern unsigned int __data_start;
+extern unsigned int __data_end;
+extern unsigned int __bss_start;
+extern unsigned int __bss_end;
+extern unsigned int __heap_start;
+extern void *__brkval;
+
+int free_memory() {
+    int free_memory;
+  
+    if((int)__brkval == 0)
+        free_memory = ((int)&free_memory) - ((int)&__bss_end);
+    else
+        free_memory = ((int)&free_memory) - ((int)__brkval);
+    return free_memory;
+};
