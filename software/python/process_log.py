1460e6e529f9b315ee20b5935462268231f6aeb4
matt venn <matt@mattvenn.net>
2015-06-04 17:45:09 +0100

    count cycles

--- software/python/process_log.py
+++ software/python/process_log.py
@@ -8,13 +8,14 @@ def process():
     line_count = 0
     last_dt = None
     last_line = ""
+    repeats = 0
     with open(args.log) as fh:
         for line in fh.readlines():
             line_count += 1
             try:
-                dline = line.split(' ')[0:2]
-                dline = ' '.join(dline)
-                dt = datetime.strptime(dline, "%m-%d %H:%M:%S")
+                line_d = ' '.join(line.split(' ')[0:2])
+                line_t = ' '.join(line.split(' ')[2:])
+                dt = datetime.strptime(line_d, "%m-%d %H:%M:%S")
             except ValueError:
                 next
             if last_dt is not None:
@@ -24,10 +25,15 @@ def process():
                     print("from: %-30s" % last_line.strip())
                     print('to  : %-30s took %s secs' % (line.strip(), delta.total_seconds()))
                     limit_lines += 1
+                if line_t == repeated_line:
+                    repeats += 1
+            else:
+                repeated_line = line_t
             last_dt = dt
             last_line = line
 
     print("%d [%d%%] lines [of %d] longer than %d" % (limit_lines, (limit_lines/float(line_count))*100,line_count, args.time))
+    print("%d cycles" % (repeats))
 
 
 if __name__ == '__main__':
@@ -35,7 +41,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="log processor")
     parser.add_argument('--time', action='store', type=int, default=5,
         help="time limit between lines")
-    parser.add_argument('--log', action='store', required=True,
+    parser.add_argument('--log', action='store', default='serial.log',
         help="log file to process")
 
     args = parser.parse_args()
