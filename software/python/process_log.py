81506725751816a46749e3ff81a93e7b063953f2
matt venn <matt@mattvenn.net>
2015-06-04 11:36:40 +0100

    args

--- software/python/process_log.py
+++ software/python/process_log.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python
 from datetime import datetime
-log_file = 'serial.log'
+import argparse
 
-limit = 5
-last_dt = None
-line_count = 0
-limit_lines = 0
-last_line = ""
-with open(log_file) as fh:
-    for line in fh.readlines():
-        line_count += 1
-        try:
-            dline = line.split(' ')[0:2]
-            dline = ' '.join(dline)
-            dt = datetime.strptime(dline, "%m-%d %H:%M:%S")
-        except ValueError:
-            next
-        if last_dt is not None:
-            delta = dt - last_dt
-            if delta.total_seconds() > limit:
-                #print delta.seconds
-                print('>' + last_line.strip())
-                print('<' + line.strip() + ' ' + str(delta.total_seconds()))
-                limit_lines += 1
-        last_dt = dt
-        last_line = line
 
-print("%d lines [of %d] longer than %d" % (limit_lines, line_count, limit))
+def process():
+    limit_lines = 0
+    line_count = 0
+    last_dt = None
+    last_line = ""
+    with open(args.log) as fh:
+        for line in fh.readlines():
+            line_count += 1
+            try:
+                dline = line.split(' ')[0:2]
+                dline = ' '.join(dline)
+                dt = datetime.strptime(dline, "%m-%d %H:%M:%S")
+            except ValueError:
+                next
+            if last_dt is not None:
+                delta = dt - last_dt
+                if delta.total_seconds() > args.time:
+                    #print delta.seconds
+                    print("from: %-30s" % last_line.strip())
+                    print('to  : %-30s took %s secs' % (line.strip(), delta.total_seconds()))
+                    limit_lines += 1
+            last_dt = dt
+            last_line = line
+
+    print("%d [%d%%] lines [of %d] longer than %d" % (limit_lines, (limit_lines/float(line_count))*100,line_count, args.time))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="log processor")
+    parser.add_argument('--time', action='store', type=int, default=5,
+        help="time limit between lines")
+    parser.add_argument('--log', action='store', required=True,
+        help="log file to process")
+
+    args = parser.parse_args()
+    process()
